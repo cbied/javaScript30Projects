dedc7afd38a884b58af5a3bf79442b10e3595826
Christopher Biediger <cabiediger@gmail.com>
2019-01-14 11:54:16 +0700

    finished array cardio 1 and flex planel

---
+++ JavaScript30-master/05 - Flex Panel Gallery/file.js
@@ -0,0 +1,9 @@
+let panels  = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+    this.classList.toggle('open');
+    this.classList.toggle('open-active');
+}
+
+panels.forEach(panel => panel.addEventListener('mouseover', toggleOpen));
+panels.forEach(panel => panel.addEventListener('mouseout', toggleOpen));
